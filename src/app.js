f7b756b42065a2d5df4b5ec3c885b68e8fbc5daf
Vinicius Fernando <viniciush.fernando@hotmail.com>
2021-11-05 22:05:21 +0000

    Add basic routing

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.get("/exchange-rate", (req, res) => {
+  res.json({
+    dollar: 5.5,
+    euro: 6.4,
+  });
+});
+app.get("/health", (req, res) => {
+  res.send("I am good");
+});
+
 module.exports = app;
